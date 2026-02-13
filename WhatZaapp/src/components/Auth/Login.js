56b95062ead8e82147dc7b6719126b2e37de7da7
IGENERALTKS <olimpio.h2so4@gmail.com>
2026-02-13 14:47:52 -0300

    Add files via upload

--- WhatZaapp/src/components/Auth/Login.js
+++ WhatZaapp/src/components/Auth/Login.js
@@ -1,44 +1,42 @@
-import React,{useState} from "react";
-import { View,Text,TextInput,TouchableOpacity,Image } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import auth from "@react-native-firebase/auth";
 import firestore from "@react-native-firebase/firestore";
-import { useNavigation} from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 
-
-export default function Login(){
-    const [phoneNumber,setNumber ] = useState("");
-    const [code,setCode] = useState("");
-    const [confirm,setConfirm] = useState(null)
+export default function Login() {
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [code, setCode] = useState("");
+    const [confirm, setConfirm] = useState(null);
     const navigation = useNavigation();
 
     const signInWithPhoneNumber = async () =>{
         try{
             //Validar formato do número de telefone//Validate phone number format
-            const phoneRegex=/^\+\d{1,4} \d{1,15}$/;
+            const phoneRegex=/^\+\d{10,15}$/;
             if(!phoneRegex.test(phoneNumber)){
-                alert("Invelid phone number format.Please enter a valid number.");
+                alert("Número inválido.Ex: (e.g., +5542988704065).");
                 return;
             }
 
             const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
             setConfirm(confirmation);
-        }
-        catch (error){
-            alert("Error sending code.Please try again later.");
-            console.log("error sending code: ",error)
+        } catch (error) {
+            alert("Erro ao enviar código. Por favor, tente novamente mais tarde.");
+            console.log("error sending code: ", error);
         }
     };
 
-    const cinbfirmCode = async () =>{
+    const confirmCode = async () =>{
         try{
             //Validar formato do código//Validate code format
             if(!code || code.length !== 6){
-                alert("Invalid code. Please enter a 6-digit code");
+                alert("Código inválido. Por favor, insira um código de 6 dígitos.");
                 return;
             }
 
             const userCredential = await confirm.confirm(code);
-            const user =userCredential.user;
+            const user = userCredential.user;
 
             //Verifique se o novo ou existente//Check if the new or existing
             const userDocument = await firestore()
@@ -49,21 +47,21 @@ export default function Login(){
             if(userDocument.exists){
                 //O usuário existe, navegue até o painel//User is existing, navagate to dashboard
                 navigation.navigate("Dashboard");
-            }else{
-                //O usuário é novo, navegue até Detalhes //User is new navigate to Detail 
-                navigation.navegate("Detail",{uid: user.uid});
+            } else {
+                navigation.navigate("Detail", { uid: user.uid });
             }
-        }catch (error){
+        } catch (error) {
             alert("Invalid code. Please enter the correct code");
-            console.log("Invalid code",error);
+            console.log("Invalid code", error);
         }
     };
     return(
         <View
             style={{
-                flex:1,
-                backgraundColor:"#000",
-                position:"relative"
+                flex: 1,
+                backgroundColor: "#000",
+                position: "relative",
+                padding: 20,
             }}
         >
             <View
@@ -91,122 +89,113 @@ export default function Login(){
             >  
                 <Text
                     style={{
-                        fontSize:32,
-                        fontWeight:"bold",
-                        marginBottom:40,
-                        marginTop:20,
-                        textAlign:"center,"
+                        fontSize: 32,
+                        fontWeight: "bold",
+                        marginBottom: 40,
+                        marginTop: 20,
+                        textAlign: "center",
                     }}
                 >
                     WhatZap 2
                 </Text>
-                {/*logo*/}
-                <View
-                    style={{
-                        alignItems:"center",
-                        justifyContent:"center",
-                        matginBottom: 30  
-                    }}
-                >
-                    <Image
-                        source={require("../../..assets/logo.png")}
-                        style={{width:150, height:150, borderRadius: 50}}
-                        />
-                </View>
+
                 {!confirm ? (
                     <>
                         <Text
                             style={{
-                                marginBottom:20,
-                                fontSize:18,
-                                color:"#808080",
+                                marginBottom: 20,
+                                fontSize: 18,
+                                color: "#808080",
                             }}
-                            >
-                                ENter your phone number with contry code :
-                            </Text>
-                            <TextInput
-                                style={{
-                                    height:50,
-                                    width:"100%",
-                                    borderColor:"black",
-                                    borderWidth:1,
-                                    marginBottom:30,
-                                    paddingHorizontal:10,
-                                    borderRadius:10
-                                }}
-                                placeholder="DDD 9 1234-5678"
-                                value={phoneNumber}
-                                onChangeText={setPhoneNumber}
-                                keyboardType="phone-pad"
-                                />
-                                <TouchableOcupacity
-                                    onPress={signInWithPhoneNumber}
-                                    style={{
-                                        backgraundColor:"#007BFF",
-                                        padding:10,
-                                        borderRadius:5,
-                                        marginBottom:20,
-                                        alignItems:"center"
-                                    }}
-                                    >
-                                        <Text
-                                            style={{
-                                                color:"white",
-                                                fontSize:22,
-                                                fontWeight:"bold"
-                                            }}
-                                        >
-                                            Verfiy Phone number    
-                                        </Text>
-                                </TouchableOcupacity>
-                    </>
-                ) : (
-                        <>
+                        >
+                               Número de telefone com código do país:
+                        </Text>
+                        <TextInput
+                            style={{
+                                height: 50,
+                                width: "100%",
+                                borderColor: "black",
+                                borderWidth: 1,
+                                marginBottom: 30,
+                                paddingHorizontal: 10,
+                                borderRadius: 10,
+                                backgroundColor: "#fff",
+                            }}
+                            placeholder="+5542912345678"
+                            value={phoneNumber}
+                            onChangeText={setPhoneNumber}
+                            keyboardType="phone-pad"
+                        />
+                        <TouchableOpacity
+                            onPress={signInWithPhoneNumber}
+                            style={{
+                                backgroundColor: "#007BFF",
+                                padding: 10,
+                                borderRadius: 5,
+                                marginBottom: 20,
+                                alignItems: "center",
+                            }}
+                        >
                             <Text
                                 style={{
-                                    marginBottom:20,
-                                    fontSize:18,
-                                    color:"#808080",
+                                    color: "white",
+                                    fontSize: 22,
+                                    fontWeight: "bold",
                                 }}
-                                >
-                                    Enter the copde sent to your phone
-                                </Text>
-                                <TextInput
+                            >
+                                Verify Phone number
+                            </Text>
+                        </TouchableOpacity>
+                    </>
+                ) : (
+                    <>
+                        <Text
+                            style={{
+                                marginBottom: 20,
+                                fontSize: 18,
+                                color: "#808080",
+                            }}
+                        >
+                            Enter the code sent to your phone
+                        </Text>
+                        <TextInput
+                            style={{
+                                height: 50,
+                                width: "100%",
+                                borderColor: "black",
+                                borderWidth: 1,
+                                marginBottom: 30,
+                                paddingHorizontal: 10,
+                                borderRadius: 10,
+                                backgroundColor: "#fff",
+                            }}
+                            placeholder="Enter code"
+                            value={code}
+                            onChangeText={setCode}
+                            keyboardType="number-pad"
+                        />
+                        <TouchableOpacity
+                            onPress={confirmCode}
+                            style={{
+                                backgroundColor: "#007BFF",
+                                padding: 10,
+                                borderRadius: 5,
+                                marginBottom: 20,
+                                alignItems: "center",
+                            }}
+                        >
+                            <Text
                                 style={{
-                                    height:50,
-                                    width:"100%",
-                                    borderColor:"black",
-                                    borderWidth:30,
-                                    marginBottom:30,
-                                    paddingHorizontal:10,
-                                    borderRadius:10,
+                                    color: "white",
+                                    fontSize: 22,
+                                    fontWeight: "bold",
                                 }}
-                                placeholder="Enter code"
-                                value={code}
-                                onChangeText={setCode}
-                                keyboardType="phone-pad"
-                                />
-                                <TouchableOpacity
-                                    onPRess={confirmCode}
-                                    style={{
-                                        backgroundColor:"#007BFF",
-                                        padding:10,
-                                        borderRadius:5,
-                                        marginBottom:20,
-                                        alignItems:"center",
-                                    }}
-                                >
-                                    <Text
-                                        style={{
-                                            color:"white",
-                                            fontSize:22,
-                                            fontWeight:"bold"
-                                        }}>
-                                            confirm Code
-                                        </Text>
-                                </TouchableOpacity>
-                        </>
-                    )}
+                            >
+                                Confirm Code
+                            </Text>
+                        </TouchableOpacity>
+                    </>
+                )}
             </View>
         </View>
     );
